5eea2376cb97cfa7c9d64cce5a299025297ebb44
Ibnurizqiaramadan <32134721+ibnurizqiaramadan@users.noreply.github.com>
2019-04-20 14:27:29 +0700

    Add files via upload

--- system/config.php
+++ system/config.php
@@ -1,14 +1,13 @@
 <?php
+session_start();
 $servername = "127.0.0.1";
 $username   = "root";
 $database   = "db_arsip_sidang";
 $password   = "";
+
 $base_url   = "http://localhost/arsipsidang/";
-$token      = md5("ibnurizqiaramadan");
+$TOKEN      = md5("ibnurizqiaramadn");
 
 $koneksi = new mysqli($servername, $username, $password, $database);
-
-$GLOBALS['koneksi']   = $koneksi;
-$GLOBALS['base_url']  = $base_url;
 
 ?>
--- system/function.php
+++ system/function.php
@@ -14,6 +14,11 @@ function base_url($url='')
   return $GLOBALS['base_url'] . $url;
 }
 
+function show_404()
+{
+  redirect(base_url('pages/404-Not-Found.php'));
+}
+
 function POST($input)
 {
   return mysqli_real_escape_string($GLOBALS['koneksi'], htmlspecialchars(rtrim(ltrim($_POST[$input]))));
@@ -36,6 +41,7 @@ function run_query($sql)
 
 function query_get_array($sql)
 {
+  $result = array();
   return mysqli_fetch_array($sql);
 }
 
@@ -51,12 +57,38 @@ function query_get_object($sql)
 
 function query_num_rows($sql)
 {
-  return mysqli_num_rows($sql);
+  return mysqli_num_rows(run_query($sql));
 }
 
 function Page($menu, $submenu, $page)
 {
   return $GLOBALS['base_url'] . "admin.php?menu=" . $menu . "&submenu=" . $submenu . "&page=" . $page;
+}
+
+function db_insert($table, $data)
+{
+  foreach ($data as $key => $value) {
+    $field = $field . "$key, ";
+    $values = $values . "'$value', ";
+  }
+  $resfield = substr($field, 0, strlen($field) - 2);
+  $resvalue = substr($values, 0, strlen($values) - 2);
+  $sql = "INSERT INTO $table ($resfield) VALUES ($resvalue);";
+  $query = run_query($sql);
+}
+
+function db_update($table, $data, $where)
+{
+  foreach ($data as $key => $value) {
+    $setvalue = $setvalue . "$key = '$value', ";
+  }
+  foreach ($where as $key => $value) {
+    $where_ = $where_ . "$key = '$value'";
+  }
+  $resvalue = substr($setvalue, 0, strlen($setvalue) - 2);
+  $sql = "UPDATE $table SET $resvalue WHERE $where_";
+  $query = run_query($sql);
+  echo "$sql";
 }
 
 ?>
